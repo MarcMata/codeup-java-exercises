fd28ab6296336b9eef602dc3c7bfa0ee2f82eb9a
Marc Mata <marcmata26@gmail.com>
2023-04-12 16:59:51 -0500

    finished OOP exercise2

--- src/shapes/circleApp.java
+++ src/shapes/circleApp.java
@@ -4,7 +4,9 @@ import util.Input;
 public class circleApp {
     public static void main(String[] args) {
         double radius = Input.getDouble();
-        System.out.println(Circle.getArea(radius));
-        System.out.println(Circle.getCircumference(radius));
+        double area = Circle.getArea(radius);
+        double circumference = Circle.getCircumference(radius);
+        System.out.printf("The area of the circle is %.2f%n", area);
+        System.out.printf("The Circumference of the circle is %.2f%n", circumference);
     }
 }
